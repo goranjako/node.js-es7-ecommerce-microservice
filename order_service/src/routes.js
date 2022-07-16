da37b5a5b5192f2e724f118a60333f01780c0b7b
Goran Jakovljević <goranjako@gmail.com>
2022-07-16 11:51:28 +0200

    auth

--- order_service/src/routes.js
+++ order_service/src/routes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 const {validateRegistrationBody,validateLoginBody,validateProductBody, validateOrderBody, validate} = require('./util/validation');
 import orderController from './controllers/order.controller';
-
+import  authManager from './util/auth';
 export default function setRoutes(app) {
 
 const router = express.Router();
-router.get("/orders", orderController.getAll);
+router.get("/orders", authManager.verifyToken,orderController.getAll);
 //productRoute
 
 app.use('/', router);
